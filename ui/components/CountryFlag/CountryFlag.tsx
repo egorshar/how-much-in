96d59fe004cb234bfc5203893c2082767dd5f589
Egor Sharapov <egor.sharapov@gmail.com>
2026-05-10 13:21:15 +0400

    Update CountryFlag.tsx

--- ui/components/CountryFlag/CountryFlag.tsx
+++ ui/components/CountryFlag/CountryFlag.tsx
@@ -46,7 +46,9 @@ export default function CountryFlag({ isoCode }: CountryFlagProps) {
         ) : null}
       </View>
 
-      <View style={tw`absolute -bottom-2 -right-1.5 bg-slate-200 px-1 rounded`}>
+      <View
+        style={tw`absolute -bottom-1.5 -right-1.5 bg-slate-200 px-1 rounded`}
+      >
         <Text style={tw`text-xs font-sans`}>{isoCodeUpper}</Text>
       </View>
     </View>
